2dbb54704ad9fd058daec2ad04cfdbd24f69666e
Matheus1206 <bandeiramatheus@hotmail.com>
2023-10-30 16:56:40 -0300

    fix: adicionando .gitignore e removendo .idea e a target

--- src/main/java/com/desafio/laboratorio/configuracao/CarregaBanco.java
+++ src/main/java/com/desafio/laboratorio/configuracao/CarregaBanco.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class LoadDatabase {
+public class CarregaBanco {
 
     @Bean
     CommandLineRunner initDatabase(ExameRepositorio exameRepositorio, LaboratorioRepositorio laboratorioRepositorio) {
